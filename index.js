cfeae63a7ecd460577caf9cbdecb08926c53e595
Steven Lu <ssl9911@gmail.com>
2020-03-14 16:04:16 -0400

    Added color picker and stroke-size

--- index.js
+++ index.js
@@ -1,43 +1,68 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+window.onload = init;
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+function init(){
 
-//Code Snippet from StackOverflow user K3N:
-//https://stackoverflow.com/questions/17130395/real-mouse-position-in-canvas
-function getMousePos(canvas, evt) {
-	var rect = canvas.getBoundingClientRect(), // abs. size of element
-	scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
-	scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
-	return {
-		x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
-		y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	var colorBtn = document.getElementById("colorBtn");
+	var strokeSizeSlider = document.getElementById("strokeSizer");
+	var strokeSizeTxt = document.getElementById("strokeSizeTxt")
+
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	//Code Snippet from StackOverflow user K3N:
+	//https://stackoverflow.com/questions/17130395/real-mouse-position-in-canvas
+	function getMousePos(canvas, evt) {
+		var rect = canvas.getBoundingClientRect(), // abs. size of element
+		scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
+		scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
+		return {
+			x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
+			y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+		}
 	}
-}
 
-penDown = false;
+	strokeSizeSlider.oninput = function sliderChangeListener(e){
+		strokeSizeTxt.value = this.value;
+		ctx.lineWidth = this.value;
+	}
 
-canvas.onmousedown = function mouseDownListener(e){
-	penDown = true;
-}
+	strokeSizeTxt.onchange = function sliderTxtChangeListener(e){
+		strokeSizeSlider.value = this.value;
+		ctx.lineWidth = this.value;
+	}
 
-canvas.onmouseup = function mouseUpListener(e){
 	penDown = false;
-}
 
-canvas.onmouseout = function mouseOutListener(e){
-	penDown = false;
-}
-
-canvas.onmousemove = function mouseMoveListener(e){
-	var pos = getMousePos(canvas,e);
-	if(penDown){
-		ctx.lineTo(pos.x,pos.y);
-		ctx.stroke();
+	canvas.onmousedown = function mouseDownListener(e){
+		penDown = true;
+		ctx.beginPath();
 	}
-	else{
-		ctx.moveTo(pos.x,pos.y);
+
+	canvas.onmouseup = function mouseUpListener(e){
+		penDown = false;
+		ctx.closePath();
+	}
+
+	canvas.onmouseout = function mouseOutListener(e){
+		penDown = false;
+		ctx.closePath();
+	}
+
+	canvas.onmousemove = function mouseMoveListener(e){
+		var pos = getMousePos(canvas,e);
+		if(penDown){
+			ctx.lineTo(pos.x,pos.y);
+			ctx.stroke();
+		}
+		else{
+			ctx.moveTo(pos.x,pos.y);
+		}
+	}
+
+	colorBtn.onchange = function colorChangeListener(e){
+		console.log(this.jscolor);
+		ctx.strokeStyle = "#" + this.jscolor;
 	}
 }
-
